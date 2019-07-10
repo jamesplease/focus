e21820a3f417ece7c4a3f07a3fc311eeb8813837
James Smith <jamesplease2@gmail.com>
2019-07-10 09:27:02 -0700

    Update exported hook name

--- src/use-set-focus.js
+++ src/use-set-focus.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import FocusContext from './focus-context';
 
-export default function useFocus() {
+export default function useSetFocus() {
   const { setFocus } = useContext(FocusContext);
 
   return setFocus;
